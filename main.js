f62920ef29e5ddf92488b934cd797363f92d123f
davide <dacapoccia@gmail.com>
2022-08-22 17:32:53 +0200

    includes()

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = new Vue({
     mounted(){
         for(let x=0; x<10; x++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then (email =>{
-                if(!this.randomEmail.includes()){
+                if(!this.randomEmail.includes(email)){
                     this.randomEmail.push(email.data.response);
                 }
             })
