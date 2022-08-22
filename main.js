985c572d0930dbac67ed0d85bcd9b9a690b991c3
davide <dacapoccia@gmail.com>
2022-08-22 16:01:25 +0200

    done

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {randomEmail:[]},
+
+    mounted(){
+        for(let x=0; x<10; x++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then (email =>{
+                if(!this.randomEmail.includes()){
+                    this.randomEmail.push(email.data.response);
+                }
+            })
+        }
+    }
+})
